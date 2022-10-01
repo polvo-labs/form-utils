0f635c5414204bc327576e4403b05c3bebf0260d
Guilherme Santiago <guilherme.santiago95@gmail.com>
2022-10-01 17:21:46 -0300

    test: add password tests

---
+++ tests/password.test.js
@@ -0,0 +1,32 @@
+import { password, passwordRequired } from "../src";
+
+test.each(["abc", ""])(
+  "value %j should be considered invalid",
+  (input) => {
+    expect(password.validate(input)).toBe(
+      "Senha precisa conter pelo menos 6 caracteres"
+    );
+  }
+);
+
+test.each(["senhaValida", "123456"])(
+  "value %j should be consided valid",
+  (input) => {
+    expect(password.validate(input)).toBeFalsy();
+    expect(passwordRequired.validate(input)).toBeFalsy();
+  }
+);
+
+test("passwordRequired requires a value", () => {
+  expect(passwordRequired.validate("")).toBe("Campo obrigatório");
+});
+
+test('[web] set the attribute type="password"', () => {
+  expect(password.web.type).toBe("password");
+  expect(passwordRequired.web.type).toBe("password");
+});
+
+test("[react native] set the prop secureTextEntry", () => {
+  expect(password.reactNative.secureTextEntry).toBe(true);
+  expect(passwordRequired.reactNative.secureTextEntry).toBe(true);
+});
